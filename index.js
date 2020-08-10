c32dc6947f7d51ead596dcfbaa7c38c66c555ac9
Sam Jeffcoat <samjeffcoat@gmail.com>
2020-08-10 11:30:28 -0500

    Data Types in JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-alert('Hello from JavaScript land');
+// Data types in JS
+
+// 1- string
+var username = '';
+var email = 'samjeffcoat@gmail.com';
+
+// 2- number
+var a = -1;
+var b = 0;
+var c = 100;
+var average = 1.98;
+
+// 3- boolean
+var javaScriptIsAwesome = true;
+var pythonIsAwesome = false;
+
+// 4 object
+var person = {
+  firstName: 'Terry',
+  lastName: 'Jones',
+  age: 37,
+};
+
+var anotherPerson = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 22,
+
+  //nested object
+  address: {
+    line1: '1 high street',
+    city: 'London',
+    countryId: 51,
+  },
+};
+
+//5 undefined
+var country;
+
+// 6- null
+//empty value
+
+var nothing = null;
+var animal = 'Hippo';
+animal = null;
+
+var user = {
+  email: 'wellpaidgeek@gmail.com',
+  age: 37,
+};
+var anotherUser = {
+  email: 'anon@test.com',
+  age: null,
+};
